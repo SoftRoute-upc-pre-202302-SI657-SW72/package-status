e3505b8c64b25edd9fd69fd9d5b2118cf65bcd0e
Yamathe5-laptop <manuelsegmar@gmail.com>
2023-10-24 01:42:38 -0500

    ADD: bootstrap properties

--- src/main/java/upc/edu/pe/softroute/trackingstatus/TrackingStatusServiceApplication.java
+++ src/main/java/upc/edu/pe/softroute/trackingstatus/TrackingStatusServiceApplication.java
@@ -2,8 +2,10 @@ package upc.edu.pe.softroute.trackingstatus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TrackingStatusServiceApplication {
 
 	public static void main(String[] args) {
